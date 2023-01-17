588a6a10194d44e6b7838a37648e1bff5e8b3e5a
Silvanos Eric <52836290+codexshell@users.noreply.github.com>
2023-01-17 15:34:17 +0300

    chore: add brand colors and fonts to website

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "brand-pink": "hsl(322, 100%, 66%)",
+        "brand-cyan-1": "hsl(193, 100%, 96%)",
+        "brand-cyan-2": "hsl(192, 100%, 9%)",
+        "brand-blue": "hsl(208, 11%, 55%)",
+      },
+      fontSize: {
+        body: "18px",
+      },
+      fontFamily: {
+        body: ["Open Sans", "sans-serif"],
+        headings: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
