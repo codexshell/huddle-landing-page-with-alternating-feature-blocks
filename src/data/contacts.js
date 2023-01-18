dbb10494d6b627bf58304b945d879ee633ece315
Silvanos Eric <52836290+codexshell@users.noreply.github.com>
2023-01-19 00:43:00 +0300

    chore: add HTML content

---
+++ src/data/contacts.js
@@ -0,0 +1,21 @@
+import locationIcon from "../assets/icon-location.svg";
+import phoneIcon from "../assets/icon-phone.svg";
+import emailIcon from "../assets/icon-email.svg";
+
+const contacts = [
+  {
+    img: locationIcon,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    img: phoneIcon,
+    description: "+1-543-123-4567",
+  },
+  {
+    img: emailIcon,
+    description: "example@fylo.com",
+  },
+];
+
+export default contacts;
---
+++ src/data/features.js
@@ -0,0 +1,32 @@
+import growTogetherImg from "../assets/illustration-grow-together.svg";
+import flowingConversationsImg from "../assets/illustration-flowing-conversation.svg";
+import yourUsersImg from "../assets/illustration-your-users.svg";
+
+const features = [
+  {
+    title: "Grow Together",
+    description: [
+      "Generate meaningful discussions with your audience and build a strong, loyal community.",
+      "Think of the insightful conversations you miss out on with a feedback form.",
+    ],
+    img: growTogetherImg,
+  },
+  {
+    title: "Flowing Conversations",
+    description: [
+      "You wouldn't paginate a conversation in real life, so why do it online?",
+      "Our threads have just-in-time loading for a more natural flow.",
+    ],
+    img: flowingConversationsImg,
+  },
+  {
+    title: "Your Users",
+    description: [
+      "It takes no time at all to integrate Huddle with your app's authentication solution.",
+      "This means, once signed in to your app, your users can start chatting immediately.",
+    ],
+    img: yourUsersImg,
+  },
+];
+
+export default features;
